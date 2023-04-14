e6b69271562799c7032687d5d575d1733a8e90a7
Avi Patel <avi.patel@hotmail.com>
2023-04-13 21:43:47 -0400

    Add courses controller

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,4 +1,4 @@
-const Student = require("../models/Student");
+const Course = require("../models/Course");
 
 module.exports = {
   index,
@@ -6,12 +6,12 @@ module.exports = {
 };
 
 async function index(req, res) {
-  // 1. Get all Students from the database
+  // 1. Get all courses from the database
   try {
-    let students = await Student.find({});
+    let courses = await Course.find({});
     // 2. Use res.json() to send the posts to the frontend
-    if (students) {
-      res.json(students).status(200);
+    if (courses) {
+      res.json(courses).status(200);
     }
   } catch (error) {
     res.status(500).json(error);
@@ -20,10 +20,10 @@ async function index(req, res) {
 
 async function create(req, res) {
   try {
-    // 1. Create a student in the database (the data will be incoming via `req.body`)
-    let student = await Student.create(req.body);
+    // 1. Create a course in the database (the data will be incoming via `req.body`)
+    let course = await Course.create(req.body);
     // 2. use res.json() to send a response to the frontend
-    if (student) {
+    if (course) {
       res.status(200).json("ok");
     }
   } catch (error) {
