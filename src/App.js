362300b47e2369d8690ebab3bc5ad925e7053d4f
Avi Patel <avi.patel@hotmail.com>
2023-04-13 22:34:37 -0400

    Resolve conflicts

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import { Route, Routes, Navigate, NavLink } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/HomePage";
 import StudentsPage from "./pages/StudentsPage/StudentsPage";
-<<<<<<< HEAD
 import CoursesPage from "./pages/CoursesPage/CoursesPage";
-=======
->>>>>>> 936eeee529f74d75efabd7171e6ed23dc5b0a46e
 
 import { getStudents } from "./utils/student";
 
@@ -25,22 +22,17 @@ function App() {
             <NavLink className="nav-link" to="/students">
               Students Page
             </NavLink>
-<<<<<<< HEAD
             <NavLink className="nav-link" to="/courses">
               Courses Page
             </NavLink>
-=======
->>>>>>> 936eeee529f74d75efabd7171e6ed23dc5b0a46e
           </div>
         </div>
         <div>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/students" element={<StudentsPage />} />
-<<<<<<< HEAD
             <Route path="/courses" element={<CoursesPage />} />
-=======
->>>>>>> 936eeee529f74d75efabd7171e6ed23dc5b0a46e
+
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </div>
